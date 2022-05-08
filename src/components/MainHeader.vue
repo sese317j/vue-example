ee628998b42d76f90727a6f3bbaf3d3f2f6877d9
Sebastian <sese.menne@web.de>
2022-05-08 14:48:21 +0200

    Deleted example files, created a main Header component and used that in the main app file.

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <Header>
+    <h1>{{ title }}</h1>
+  </Header>
+</template>
+
+<script>
+export default {
+  name: "MainHeader",
+  props: ["title"],
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
