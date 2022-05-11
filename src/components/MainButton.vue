ac433f132a534a87205d6058ba3c8f995f61c411
Sebastian <sese.menne@web.de>
2022-05-11 13:07:59 +0200

    Added Button Component and used it in the header

---
+++ src/components/MainButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button :style="{ background: color }" class="btn">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "MainButton",
+  props: {
+    color: String,
+    text: String,
+  },
+};
+</script>
+
+<style scoped></style>
--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,13 +1,21 @@
 <template>
   <Header>
     <h1>{{ title }}</h1>
+    <MainButton text="Add Task" color="green"></MainButton>
   </Header>
 </template>
 
 <script>
+import MainButton from "@/components/MainButton";
+
 export default {
   name: "MainHeader",
-  props: ["title"],
+  props: {
+    title: String,
+  },
+  components: {
+    MainButton,
+  },
 };
 </script>
 
