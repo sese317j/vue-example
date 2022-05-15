e9a9c8d9d8105abcbeb301034b3113b903131fbf
Sebastian <sese.menne@web.de>
2022-05-15 14:41:37 +0200

    Added TaskItem component which represents a single task. Added Styling (also font awesome) and used the new task item component for the task list.

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,12 +1,19 @@
 <template>
-  <h1>test</h1>
+  <div :key="task.id" v-for="task in tasks">
+    <task-item :task="task" />
+  </div>
 </template>
 
 <script>
+import TaskItem from "@/components/TaskItem";
+
 export default {
   name: "TaskList",
   props: {
     tasks: Array,
+  },
+  components: {
+    TaskItem,
   },
 };
 </script>
